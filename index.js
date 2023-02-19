5056a1639cb9325437984cb8f40175bb40b5ed2c
Gessio Mori <gessiomori@protonmail.com>
2023-02-19 09:57:32 -0300

    adds host 4

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const host = "localhost";
+const host = "cors-gm.herokuapp.com";
 const port = process.env.PORT || 8080;
 
 const cors_proxy = require("cors-anywhere");
 cors_proxy
   .createServer({
-    originWhitelist: ["https://cors-anywhere-production-5311.up.railway.app"],
+    originWhitelist: [],
     requireHeader: ["origin", "x-requested-with"],
     removeHeaders: ["cookie", "cookie2"],
   })
