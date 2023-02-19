2e63d29132829e85cdaf52eb798d8da1be2f8f3c
Gessio Mori <gessiomori@protonmail.com>
2023-02-19 09:45:46 -0300

    adds host 2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const host = "https://cors-anywhere-production-5311.up.railway.app/";
+const host = "cors-anywhere-production-5311.up.railway.app";
 const port = process.env.PORT || 8080;
 
 const cors_proxy = require("cors-anywhere");
