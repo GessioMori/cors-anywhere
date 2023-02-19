ff1fd71e15c6c3efe0d7ad468f47842ea1f48797
Gessio Mori <gessiomori@protonmail.com>
2023-02-19 09:44:11 -0300

    adds whitelist 2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const host = process.env.HOST || "localhost";
+const host = "https://cors-anywhere-production-5311.up.railway.app/";
 const port = process.env.PORT || 8080;
 
 const cors_proxy = require("cors-anywhere");
